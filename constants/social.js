ac24f05ead5ce0ff8eea6996ba25b002f30c3543
Plastic Digits <plasticdigits@protonmail.com>
2023-07-02 01:12:03 -0700

    fix links

--- constants/social.js
+++ constants/social.js
@@ -1,5 +1,5 @@
-export const SOCIAL_TWITTER = "https://twitter.com/AtomicWealthAW";
-export const SOCIAL_TELEGRAM = "https://t.me/AtomicWealthportal";
+export const SOCIAL_TWITTER = "https://twitter.com/drxdefi/status/1664773177514373121?s=46&t=CUwmWDRRURH-NDvGc3CsBw";
+export const SOCIAL_TELEGRAM = "https://t.me/drxchat";
 export const SOCIAL_GITHUB = "https://github.com/chinese-zodiac";
 
 export const LINK_TERMS_OF_USE = "https://czodiac.gitbook.io/czodiac-litepaper/terms-of-use";
